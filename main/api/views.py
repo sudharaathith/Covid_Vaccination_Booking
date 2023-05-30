ff0cdc2f91c9b710c3f7349ae507e0bf1f22b1cb
sudharaathith <t.sudharaathith@gmail.com>
2023-05-30 07:11:43 +0530

    init jws tokens

--- main/api/views.py
+++ main/api/views.py
@@ -1,5 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 @api_view(['GET'])
 def getRoutes(request):
