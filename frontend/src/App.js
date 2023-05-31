b4db450198e0d8e1a9b166cc0ee35ebbc77f281c
sudharaathith <t.sudharaathith@gmail.com>
2023-05-31 19:59:27 +0530

    nav and init privit Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import HomePage from './Page/HomePage';
+import LoginPage from './Page/LoginPage';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <p>basic react app</p>
+      <BrowserRouter>
+        <NavBar />
+      <Routes>
+        <Route element={<HomePage />} path='/' exact/>
+        <Route element={<LoginPage />} path='/login'/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
